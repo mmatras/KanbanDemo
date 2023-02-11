460c4c51db07152047d9d2305d1ab1b451e50cc6
Daniel Matras <mmatras@gmail.com>
2023-02-11 14:35:26 +0100

    added person edit

--- DemoKanban/Controllers/PersonController.cs
+++ DemoKanban/Controllers/PersonController.cs
@@ -1,21 +1,66 @@
 ﻿using DemoKanban.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace DemoKanban.Controllers
 {
     public class PersonController : Controller
     {
-        private readonly KanbanContext _kanbanContext;
+        private readonly KanbanContext _context;
 
         public PersonController(KanbanContext kanbanContext)
         {
-            _kanbanContext = kanbanContext;
+            _context = kanbanContext;
         }
 
         public IActionResult Index()
         {
-            var people = _kanbanContext.People.ToList();
+            var people = _context.People.ToList();
             return View(people);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var issue = _context.People.FirstOrDefault(i => i.Id == id);
+
+            if (issue == null)
+            {
+                return RedirectToAction("Error404", "Error");
+            }
+
+            return View(issue);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, [FromForm] Person person)
+        {
+            if (id != person.Id)
+            {
+                return RedirectToAction("Error400", "Error");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return View(person);
+            }
+
+            var personToBeupdated = _context.People.FirstOrDefault(i => i.Id == id);
+
+            if (personToBeupdated == null)
+            {
+                return RedirectToAction("Error400", "Error");
+            }
+
+            personToBeupdated.Name = person.Name;
+            personToBeupdated.Surname = person.Surname;
+            personToBeupdated.DateOfBirth = person.DateOfBirth;
+            personToBeupdated.DisplayName = person.DisplayName;
+
+            _context.People.Update(personToBeupdated);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index");
         }
     }
 }
